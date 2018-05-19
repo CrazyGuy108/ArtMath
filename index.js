adf0f2aa6d029f4bf5cf5594ab2e930b10694696
Rahul S. Deshpande <rahulsachindeshpande@gmail.com>
2018-05-19 14:39:56 -0700

    change brace style

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const DIM = 1024;
-var fractals = {
+var fractals =
+{
     oc: {
         r:(i, j) => j^j-i^i,
         g: (i,j) => (i-DIM)^2+(j-DIM)^2,
@@ -7,19 +8,24 @@ var fractals = {
     }
 };
 
-window.onload = function () {
+window.onload = function ()
+{
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
 
-    document.getElementById("draw").addEventListener("click", function() {
+    document.getElementById("draw").addEventListener("click", function()
+    {
         draw(500, 500);
     });
 
     var f = fractals.oc;
 
-    function draw(width, height){
-        for (let i = 0; i < width; i++) {
-            for (let j = 0; j < height; j++) {
+    function draw(width, height)
+    {
+        for (let i = 0; i < width; i++)
+        {
+            for (let j = 0; j < height; j++)
+            {
                 ctx.fillStyle = `rgb(${f.r(i, j)}, ${f.g(i, j)}, ${f.b(i, j)})`;
                 ctx.fillRect(i, j, 1, 1);
             }
